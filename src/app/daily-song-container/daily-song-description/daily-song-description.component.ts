48af2d7273b6888d53b54be322825ee0aef41351
AndresDoncel <andresdoncel95@hotmail.com>
2023-05-09 15:40:26 -0600

    minor change

--- src/app/daily-song-container/daily-song-description/daily-song-description.component.ts
+++ src/app/daily-song-container/daily-song-description/daily-song-description.component.ts
@@ -11,7 +11,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
     Tienes la oportunidad de adivinar el título de una canción, ¡pero tienes solo tres intentos! Con cada intento,
     el tiempo de la canción aumentará unos segundos, pero ganarás menos puntos.
    </p>
-    <p class="text-base font-extrabold text-gray-900 dark:text-white"><span
+    <p *ngIf="!startDaily" class="text-base font-extrabold text-gray-900 dark:text-white"><span
     class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">¿Estás listo para el desafío musical?</span>
     </p>
     <button
