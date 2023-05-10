ac785a4463eaf13e3b568956be6cfcb1b25c5e4a
AndresDoncel <andresdoncel95@hotmail.com>
2023-05-09 18:00:35 -0600

    fix footer

--- src/app/daily-song-container/daily-song-footer/daily-song-footer.component.ts
+++ src/app/daily-song-container/daily-song-footer/daily-song-footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-daily-song-footer',
   template: `
-  <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-center md:p-6 dark:bg-gray-800 dark:border-gray-600">
+  <footer class="text-center fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-center md:p-6 dark:bg-gray-800 dark:border-gray-600">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/AndresDoncel/guess-daily-song" target="_blank" class="hover:underline">Made with Love ❤️ Andres Doncel</a>
     </span>
   </footer>
