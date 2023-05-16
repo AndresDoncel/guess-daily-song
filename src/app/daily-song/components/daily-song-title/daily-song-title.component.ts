acb9bd1bba93b77887098dc8fc23e80f534509f3
andres_doncel <andres_doncel@epam.com>
2023-05-16 10:49:54 -0600

    fix padding

--- src/app/daily-song/components/daily-song-title/daily-song-title.component.ts
+++ src/app/daily-song/components/daily-song-title/daily-song-title.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-daily-song-title',
   template: `
-    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl"><span
+    <h1 class="px-6 pt-4 mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl"><span
     class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Guess the song</span> Daily.
     </h1>
   `,
